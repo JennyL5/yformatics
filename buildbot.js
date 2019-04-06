02f1e0f76e1fc396f87574d8aa78a6fa154a3692
macleos <samuel@macleod.space>
2019-04-06 01:23:47 +0100

    Update buildbot.js

--- buildbot.js
+++ buildbot.js
@@ -1,1 +1,1 @@
-module.exports = { port: 8000, hosts: ["yformatics.muel.us", "y.muel.us"] }
+module.exports = { port: 8000, hosts: ["yformatics.muel.us", "y.muel.us", "yformatics.com", "www.yformatics.com"] }
